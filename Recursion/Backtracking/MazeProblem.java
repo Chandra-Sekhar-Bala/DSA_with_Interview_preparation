634cab7af907827e09195a49c534ad56e32a68db
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-06-17 01:08:24 +0530

    Maze problmes are fun

--- Recursion/Backtracking/MazeProblem.java
+++ Recursion/Backtracking/MazeProblem.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+
+/*
+ * 
+ * 
+ * 
+ */
+
 public class MazeProblem {
     public static void main(String[] args) {
+
 
         // System.out.println(Count(3, 3));
 
@@ -9,12 +18,166 @@ public class MazeProblem {
             {7,8,9}
         };
 
-        System.out.println(Counting(a, 0, 0));
+        boolean maze [][] = {
+            {true, true, true},
+            {true, true, true},
+            {true, true, true}
+        };
+        int [][] board = new int [maze.length][maze[0].length];
+
+        // System.out.println(Counting(a, 0, 0));
+        // System.out.println(CountingPaths( "", a, 0, 0));
+        // System.out.println(WithDiagonalPaths( "", a, 0, 0));
+        // System.out.println("Totoal paths : "+printAllPath("",maze, a,0,0));
+        MatrixPath("", maze, board, a, 0, 0,1);
         
     }
 
-    static int Counting(int [][] a, int r, int c){
+     static void MatrixPath(String path, boolean[][]maze, int board[][],int [][] a, int r, int c, int step){ //
 
+        if(!maze[r][c]){ // if this is already visited then fall back
+            return ;
+        }
+
+        // Putting the step value :Here cz the last cell will also have values
+        board[r][c] = step;
+         
+        if(r == a.length -1 && c == a[0].length-1 ){ // reach the last cell then print
+          for(int []x : board){
+            System.out.println(Arrays.toString(x));
+          }
+          System.out.println();
+            return ;
+        }
+
+        // marking current pos as visited
+        maze[r][c] = false;
+
+
+        if(r > 0 ){ // Top case
+           MatrixPath(path+'T',maze,board, a, r-1, c, step+1);
+        }
+
+        if(c < a[0].length -1){ // right
+             MatrixPath(path+'R',maze,board, a, r, c+1,step+1);
+        }
+       
+        if(c > 0 ){ // Left case
+           MatrixPath(path+'L',maze,board, a, r, c-1,step+1);
+        }
+
+        if(r < a.length -1){ // down
+            MatrixPath(path+'D',maze,board, a, r+1, c,step+1);
+        }
+
+        // if(r < a.length -1 && c < a[0].length-1){ // Diagonal
+        //    calls += printAllPath(path+'d',maze, a, r+1, c+1);
+        // }
+        
+        // Backtracking part : Comeback and erase 
+        // While coming back from down recurion calls all the changes that were made should be erased
+        maze[r][c] = true;
+        board[r][c] = 0;
+
+       return;
+    }
+
+    static int printAllPath(String path,boolean[][]maze, int [][] a, int r, int c){ //
+
+        if(!maze[r][c]){ // if this is already visited then fall back
+            return 0;
+        }
+        
+        if(r == a.length -1 && c == a[0].length-1 ){ // reach the last cell then print
+            System.out.println(path);
+            return 1;
+        }
+
+        // marking current pos as visited
+        maze[r][c] = false;
+
+        // totla paths count
+        int calls = 0;
+
+        if(r > 0 ){ // Top case
+           calls += printAllPath(path+'T',maze, a, r-1, c);
+        }
+
+        if(c < a[0].length -1){ // right
+            calls += printAllPath(path+'R',maze, a, r, c+1);
+        }
+       
+        if(c > 0 ){ // Left case
+           calls += printAllPath(path+'L',maze, a, r, c-1);
+        }
+
+        if(r < a.length -1){ // down
+            calls += printAllPath(path+'D',maze, a, r+1, c);
+        }
+
+        // if(r < a.length -1 && c < a[0].length-1){ // Diagonal
+        //    calls += printAllPath(path+'d',maze, a, r+1, c+1);
+        // }
+        
+        // Backtracking part : Comeback and erase 
+        // While coming back from down recurion calls all the changes that were made should be erased
+        maze[r][c] = true;
+       return calls;
+    }
+
+
+
+
+  // Shows the Actual path eg : dRRD || dd
+    static int WithDiagonalPaths(String path, int [][] a, int r, int c){ //
+        
+        if(r == a.length-1 && c == a[0].length-1 ){
+            System.out.println(path);
+            return 1;
+        }
+        int total = 0;
+
+        if(r < a.length-1 ){ // For going Down
+            total += WithDiagonalPaths(path + 'D', a, ++r, c); --r;
+        }
+
+          if(c < a[0].length-1 && r < a.length-1){ // If reaches the edge cannot move diagonally 
+            total += WithDiagonalPaths(path + 'd', a, ++r, ++c); --r; --c;
+        }
+
+        if(c < a[0].length-1 ){ // For going Right
+            total += WithDiagonalPaths(path + 'R', a, r, ++c); --c;
+        }
+
+        return total;
+    }
+
+
+    
+    // Shows the Actual path eg : DRRD
+    static int CountingPaths(String path, int [][] a, int r, int c){ //
+        
+        if(r == a.length-1 && c == a[0].length-1 ){
+            System.out.println(path);
+            return 1;
+        }
+        int total = 0;
+
+        if(r < a.length-1 ){ // For going Down
+            total += CountingPaths(path + 'D', a, ++r, c); --r;
+        }
+
+        if(c < a[0].length-1 ){ // For going R
+            total += CountingPaths(path + 'R', a, r, ++c); --c;
+        }
+
+        return total;
+    }
+
+    // Shows the Actual path eg : DRRD
+    static int Counting(int [][] a, int r, int c){ //
+
+        //if row or colum is at the end then only one way to reach destination
         if(r == a.length-1 || c == a[0].length-1 ){
             return 1;
         }
@@ -30,8 +193,9 @@ public class MazeProblem {
         return down + right;
     }
 
-    static int Count(int r, int c){
+    static int Count(int r, int c){ // Only can move to right and down 
 
+        //if row or colum is at the end then only one way to reach destination
         if(r == 1 || c == 1){
             return 1;
         }
