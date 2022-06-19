db0b34048691190c062ac560d22f76f3b49fcaef
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-06-19 14:59:19 +0530

    N-Queen thought process (Notes) added

--- Recursion/Backtracking/NQueen.java
+++ Recursion/Backtracking/NQueen.java
@@ -1,7 +1,46 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ArrayBlockingQueue;
+
+/*
+ *** What is N-Queen?
+ * - Given a NxN matrix - put a queen at each row. 
+ *** Condition:
+ *  -Queen can move  right, left and dialonally i,e One row can contaion one queen
+ * Place the queen that way they do't eliminate themselves
+ * 
+ *** Thought prrocess: ***
+ * -> Is there is need of recursion??
+ *  Yes, as I can break it into smaller problem
+ *      - I can place a queen at I'th row and move down, now worry about i-1 row and so on
+ * 
+ *** -> Parameters in recursion call?
+ *  - The board and the row - col can be reduced with loop
+ *  **-> Base case:
+ *      - When my row exceeded the size ( row == board.length ), that means all queens has been placed
+ *  
+ *** -> Recursion call ?
+ *  - Move column from 0 to size if the board.length.
+ *  - At each position (row,col) check if it safe to place a queen here?
+ *  - If (yes):
+ *      place the queen here
+ *      Break down the problmes to (row+1) ans so on
+ *     *** Am i missing something?
+ *          - Suppose the last row left to place queen. But the last row cannot contain a queen because of previous Queens position
+ *            So I need to check for other place to put a queen i.e (current-1'th) row. But as I've already placed a queen, it
+ *            will be ambiguous to put another one at the same row. That's why while we backtrack clear all the changes made by 
+ *            that call;
+ * 
+ *          - While coming back from recusrion stack I've to remove queen from that position
+ *  - else
+ *      - Move on without making andy changes
+ * 
+ ***-> isSafe() function :
+ *      - Current row will not have any Queen 
+ *      - No need to check for below as queen hasen't been palced there yet
+ *      - Chec for top columns, left and right diagonal :
+ *          - Top cols : row 0 to current and col is current column
+ *          - Right diagonal : row and column is decrested until any of them become 0
+ *          - Left diagonal : row is decresed untill 0, and column is incresded unitil the last column 
+ */
 
 public class NQueen {
 
