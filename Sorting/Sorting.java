396a7d3f6af704af113fc7d5148efbc52c7a7928
Chandra Sekhar <sekhar.chandra.5832@gmail.com>
2022-08-06 20:26:31 +0530

    Quick_sort + comments added

--- Sorting/Sorting.java
+++ Sorting/Sorting.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 class Sorting{
 
     public static void main(String [] kichu){
-        int [] arr = {6,5,4,3,7};
-        BubbleSort(arr);
+        int [] arr = {7,6,5,3,4,2,1};
+        // BubbleSort(arr);
+        quick_sort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
         
     }
@@ -35,8 +36,6 @@ class Sorting{
         /**
          *  Select an element and put it at it's right position
          */
-
-        
          for(int i = 0; i < n-1 ; ++i){
              int maxIndex  = i;
              
@@ -48,6 +47,44 @@ class Sorting{
 
         }
     }
+
+    /*
+     * Not stable | In place (O(1)) 
+     * This holds a emenet called pivot : any element
+     * Intution : Move all the element great than  pivot ro right and smaller to left:
+     * check for the left and right side of pivot     
+     * 
+     */
+    static void quick_sort(int num[], int low, int hi){
+        
+        if(low >= hi)
+            return;
+
+        int s = low; 
+        int e = hi;
+        int mid = s + (e - s)/2;
+        int pivot = num[mid];
+
+        while( s <= e){
+            // if array is alreay sort it will not sort it:
+            while(num[s] < pivot) s++;
+            while(num[e] > pivot) e--;
+
+            if(s <= e){
+                int temp = num[s];
+                num[s++] = num[e];
+                num[e--] = temp;
+            }
+        }
+        // Pivot is at correct : sort the two halfs
+
+        // Not sending **s+1 or e-1** cz suppose s and e was pointing at pivot and it swapped with it then s pointing to pivot+1 and end to pivot-1, if again do +1 or -1 that element is missing out!!!
+        quick_sort(num, low, e); 
+        quick_sort(num, s, hi);
+
+    }
+
+
     private static void swap(int[] arr, int pos1, int pos2) {
         int temp = arr[pos1];
         arr[pos1] = arr[pos2];
